bc20a61bc8dea8a22a0d3c4ee1bc53cf79b61b47
Janith Madarasinghe <janithpm9991@gmail.com>
2023-07-05 11:27:12 +0530

    new pg response added

--- src/pages/confirm/ConfirmPage.jsx
+++ src/pages/confirm/ConfirmPage.jsx
@@ -1,10 +1,38 @@
 import logo from "../../assets/img/logo-crop.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { CheckCircleFill } from "react-bootstrap-icons";
-
+import { useSearchParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
+// http://127.0.0.1:5173/payment-confirm?clientRef=CREFT2-12345&reqid=UEwEKbA1QnirORnt2S4Z
+// 'https://sampath.paycorp.lk/webinterface/qw/confirm?csrfToken=' . $csrfToken . '&authToken=' . $auth_Token . '&clientRef=' . $clientRef
+
 function ConfirmPage() {
+    const [searchParams] = useSearchParams()
+    const clientRef = searchParams.get('clientRef')
+    const reqid = searchParams.get('reqid')
+
+    const [data, setData] = useState([])
+
+    const getPayment = async () => {
+        try {
+            const response = await axios.post(`https://sampath.paycorp.lk/webinterface/qw/confirm?csrfToken=${reqid}&authToken=${import.meta.env.VITE_PG_AUTH_TOKEN}&clientRef=${clientRef}`)
+            setData(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getPayment()
+    }, [clientRef, reqid])
+
+    console.log(data);
+
+
     return (
+
         <>
             <section>
                 <nav className="navbar navbar-expand-lg bg-inverse scrolling-navbar top-nav-collapse">
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,5 +1,8 @@
 import RegisterForm from "./components/RegisterForm";
+
+
 function index() {
+
   return(
     <>
     <RegisterForm />
